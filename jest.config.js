1df40f1cba6d86a686c7220f9c0513622c229265
Kamaal Farah <kamaal.f1@gmail.com>
2026-07-26 23:24:22 +0200

    Migrate test suite from Jest to Vitest
    Switch to Vitest's globals mode to keep test files unchanged, replacing
    ts-jest/babel-jest with native TS support. Also drops the now-unused
    ts-node dependency.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-import { createDefaultPreset } from 'ts-jest';
-
-const tsJestPreset = createDefaultPreset({ tsconfig: 'tsconfig.tests.json' });
-
-/** @type {import("jest").Config} **/
-export default {
-  testEnvironment: 'node',
-  transform: tsJestPreset.transform,
-};
