287582c8b65666d834aa51ea891264235c7ad415
Kamaal Farah <kamaal.f1@gmail.com>
2026-07-26 23:26:46 +0200

    Use pnpm in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,5 +9,5 @@ echo "🐸 $VERSION"
 pnpm i
 rm -rf dist
 pnpm build
-npx tsx scripts/deployment-package-json.ts "${VERSION:-null}"
-npm publish --access public
+pnpm exec tsx scripts/deployment-package-json.ts "${VERSION:-null}"
+pnpm publish --access public
